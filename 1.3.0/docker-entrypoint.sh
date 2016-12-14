4a7a2ecbe954b1889c5adaa952b122228351d406
will Farrell <will.farrell@gmail.com>
2016-12-13 18:49:13 -0700

    fix handling of env

--- 1.3.0/docker-entrypoint.sh
+++ 1.3.0/docker-entrypoint.sh
@@ -25,11 +25,6 @@ if [ "$CONFIG" = '' ]; then
 	CONFIG=/filebeat.yml
 fi
 
-# Render config file
-cat $CONFIG | sed "s/LOGSTASH_HOST/$LOGSTASH_HOST/" | sed "s/LOGSTASH_PORT/$LOGSTASH_PORT/" | sed "s/INDEX/$INDEX/" > $CONFIG.tmp
-cat $CONFIG.tmp > $CONFIG
-rm $CONFIG.tmp
-
 # Add filebeat as command if needed
 if [ "${COMMAND:0:1}" = '-' ]; then
 	COMMAND="filebeat $COMMAND"
